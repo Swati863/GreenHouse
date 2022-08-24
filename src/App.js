99ab7d362ff3abd3796664f75ddc16ceb70de963
AwsomeSwati <swati1993kushwaha@gmail.com>
2022-08-24 23:02:25 +0530

    changes in homepage

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Greenhouse />
-      {/* <AllRoutes /> */}
+      {/* <Greenhouse /> */}
+      <AllRoutes />
      
     </div>
   );
